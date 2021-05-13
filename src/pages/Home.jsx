bbc3b14e7748623858796ca4aef4a252f8aab5c7
guosenquan <guosenquan@supcon.com>
2021-05-13 09:50:38 +0800

    完成库存部分

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,58 +1,76 @@
 import React from 'react'
 import './Home.scss'
 import headerBg from '../img/header.png'
-import Chart2 from '../components/DeviceProcessingTable'
-import Chart3 from '../components/Yield/PieChart1'
-import Chart4 from '../components/Yield/PieChart2'
-import Chart5 from '../components/Yield/PieChart3'
-import Chart6 from '../components/ProductInventoryChart/BarChart'
+import DeviceProcessingTable from '../components/DeviceProcessingTable'
+import PieChart1 from '../components/Yield/PieChart1'
+import PieChart2 from '../components/Yield/PieChart2'
+import PieChart3 from '../components/Yield/PieChart3'
+import BarChart from '../components/ProductInventoryChart/BarChart'
 // import Chart7 from '../components/Chart7'
-import Chart8 from '../components/ProductSalesTable'
-import Chart9 from '../components/Benefit/LineChart'
+import ProductSalesTable from '../components/ProductSalesTable'
+import LineChart from '../components/Benefit/LineChart'
 
 export default function Home () {
   return (
     <div className='home'>
-      <header style={{ backgroundImage: `url(${headerBg})` }}>
-        supAPS
-      </header>
+      <header style={{ backgroundImage: `url(${headerBg})` }}>supAPS</header>
 
       <main>
         <section className='bordered section1'>
-          <div className='yuanliaokucun'>
+          <div className='product-inventory'>
             <div className='yuanliao1'>
-                <span className="spin">绝对定位</span>
-                <span className="库存名称">绝对定位 参考text</span>
-                <span className="bordered 库存量">边框效果 大小固定 绝对定位</span>
+              <span className='yuanliao spin'></span>
+              <span className='yuanliao inventory-title'>原料1</span>
+              <span className='bordered yuanliao inventory-value'>
+                库存量1111
+              </span>
             </div>
-            <div className='yuanliao2'></div>
-            <div className='yuanliao3'></div>
-            <div className='yuanliao4'></div>
+            <div className='yuanliao2'>
+              <span className='yuanliao spin'></span>
+              <span className='yuanliao inventory-title'>原料2</span>
+              <span className='bordered yuanliao inventory-value'>
+                库存量2222
+              </span>
+            </div>
+            <div className='yuanliao3'>
+              <span className='yuanliao spin'></span>
+              <span className='yuanliao inventory-title'>原料3</span>
+              <span className='bordered yuanliao inventory-value'>
+                库存量3333
+              </span>
+            </div>
+            <div className='yuanliao4'>
+              <span className='yuanliao spin'></span>
+              <span className='yuanliao inventory-title'>原料4</span>
+              <span className='bordered yuanliao inventory-value'>
+                库存量4444
+              </span>
+            </div>
           </div>
         </section>
         <section className='section2'>
-          <Chart2 />
+          <DeviceProcessingTable />
         </section>
         <section className='section3'>
           <div className='bordered yield'>
             <h2>收率</h2>
             <div className='charts'>
-              <Chart3 />
-              <Chart4 />
-              <Chart5 />
+              <PieChart1 />
+              <PieChart2 />
+              <PieChart3 />
             </div>
           </div>
         </section>
         <section className='section4'>
-          <Chart6 />
+          <BarChart />
         </section>
         <section className='section5'>
-          <Chart8 />
+          <ProductSalesTable />
         </section>
         <section className='section6'>{/* <Chart7 /> */}</section>
         <section className='bordered section7'></section>
         <section className='section8'>
-          <Chart9 />
+          <LineChart />
         </section>
       </main>
     </div>
